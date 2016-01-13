decda054f09e14aff3a15bf191996c910596817f
Thulasidhar2015 <thulasidhar2012@gmail.com>
2016-01-13 19:48:28 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="ie ie6 no-js" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 no-js" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 no-js" lang="en"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 no-js" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <title>Flash- Categories</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content=/>
+        <meta name="keywords" content="/>
+        <meta name="author" content="/>
+        <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/flash.css" />
+		<script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+		<script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+		<script type="text/javascript" src='js/search.js'></script>
+		<style>
+		.frmSearch{float:right;position:relative;z-index:2px;bottom:40px;}
+#country-list{float:left;list-style:none;margin:0;padding:0;width:422px;position:absolute;}
+#country-list li{padding: 10px; background:#FAFAFA;border-bottom:#F0F0F0 1px solid;}
+#country-list li:hover{background:#F0F0F0;}
+#search-box{padding: 10px;border: #F0F0F0 1px solid;position:relative;width:400px;}
+		</style>
+    </head>
+    <body id="page">
+        <ul class="cb-slideshow">
+            <li><span>Image 01</span></li>
+            <li><span>Image 02</span></li>
+            <li><span>Image 03</span></li>
+            <li><span>Image 04</span></li>
+            <li><span>Image 05</span></li>
+            <li><span>Image 06</span><div><h3>re·lax·a·tion</h3></div></li>
+        </ul>
+        <div class="container">
+            <header>
+                <span style='font-size:40px;color:white;font-family:comic sans ms;'>Flash</span>
+                <h2>A Newly Startup</h2>
+				
+				<div class="frmSearch">
+				<form action='search.php' method='get'>
+			<input type="text" id="search-box" name='search' placeholder="Search Here" size='50'/>
+			<input style='width:100px;border:1px solid #8f0222;color:white;border-radius:4px;background:#8f0222;padding:7px;font-family:comic sans ms;font-size:15px;' type='submit' name='submit' value='search'>
+			</form>
+			<div id="suggesstion-box"></div>
+			</div>
+            </header>
+		
+			<p class="codrops-demos">
+					<a style='padding:6px 35px 9px;' href="index.php">Home</a>
+					<a style='padding:6px 35px 9px;' href='categories.php'>Categories</a>
+					<a style='padding:6px 35px 9px;' href="about.php">About</a>
+					<a style='padding:6px 31px 9px;' href="contact.php">Contact</a>
+				</p>
+	
+			</div>
+       
+    </body>
+</html>
