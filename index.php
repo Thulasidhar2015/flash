17a784daff932abc02338824d0906f27ac0d9e9c
Thulasidhar2015 <thulasidhar2012@gmail.com>
2016-01-15 09:06:21 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <title>Flash- Categories</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content=/>
+        <meta name="keywords" content="/>
+         <link rel="icon" type="image/png" href="images/icon.png"/>
+		 <link rel="Shortcut icon" href="images/icon.png">
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/flash.css" />
+		<script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+		<script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+		<script type="text/javascript" src='js/search.js'></script>
+    </head>
+    <body id="page">
+        <ul class="cb-slideshow">
+            <li><span>Image 01</span><div><h3>Compare and Decide</h3></div></li>
+            <li><span>Image 02</span><div><h3>Save Money</h3></div></li>
+            <li><span>Image 03</span><div><h3>Save Time</h3></div></li>
+            <li><span>Image 04</span><div><h3>Surf Less Get More</h3></div></li>
+            <li><span>Image 05</span><div><h3>Go For the Best</h3></div></li>
+            <li><span>Image 06</span><div><h3>..Shop Smartly</h3></div></li>
+        </ul>
+        <div class="container">
+            <header>
+                <div style='height:150px;width:110px;float:left;'>
+					<img src='images/logo.jpg' height=150>
+					<h2 style='font-size:17px;font-family:cursive;float:left;'>Shop Smartly</h2>
+				</div>
+              	<p class="codrops-demos" style='float:right'>
+					<a style='padding:6px 35px 9px;' href="index.php">Home</a>
+					<a style='padding:6px 35px 9px;' href='products.php'>Products</a>
+					<a style='padding:6px 35px 9px;' href="about.php">About</a>
+					<a style='padding:6px 31px 9px;' href="contact.php">Contact</a>
+				</p>
+				</header><center>
+		<div class="frmSearch">
+				<form action='search.php' method='get'>
+			<input type="text" id="search-box" name='search' placeholder="Search Here" size='50'/>
+			<input style='width:100px;border:1px solid #8f0222;color:white;border-radius:4px;background:#8f0222;padding:7px;font-family:comic sans ms;font-size:15px;' type='submit' name='submit' value='search'>
+			</form>
+			<div id="suggesstion-box"></div>
+			</div>
+			</div></center>
+           </body>
+</html>
