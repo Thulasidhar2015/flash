9a0e3b914525a6b9171de288298adad2b73a4fea
Thulasidhar2015 <thulasidhar2012@gmail.com>
2016-01-15 09:03:10 +0530

    Create prices.php

---
+++ pageportion/prices.php
@@ -0,0 +1,23 @@
+<div class="different_filters_divbox">                                            
+	<ul class="different_filters">
+			
+		<li>
+			<input type="radio" name="price_range" id="price_range1" value="1000-3000" class="price_range"/>
+			<label for="price_range1">Rs. 1000 - Rs. 3000</label>
+		</li>
+		<li>
+			<input type="radio" name="price_range" id="price_range2" value="3000-5000" class="price_range"/>
+			<label for="price_range2">Rs. 3000 - Rs. 5000</label>
+		</li>
+		<li>
+			<input type="radio" name="price_range" id="price_range3" value="5000-25000" class="price_range"/>
+			<label for="price_range3">Rs. 5000 - Rs. 25000</label>
+		</li>
+		<li>
+			<input type="radio" name="price_range" id="price_range4" value="25000-60000" class="price_range"/>
+			<label for="price_range4">Rs. 25000 - Rs. 60000</label>
+		</li>
+		
+		
+	</ul>
+</div>
